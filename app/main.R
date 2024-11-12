1d3395584fa173b2dcf35fd92dcebf94afde5a8d
davidrsch <daviddrsch@gmail.com>
2024-11-12 22:06:25 +0100

    Adding sidebar to main

--- app/main.R
+++ app/main.R
@@ -1,25 +1,45 @@
 box::use(
-  shiny[bootstrapPage, div, moduleServer, NS, renderUI, tags, uiOutput],
+  shiny.fluent[fluentPage, Pivot, PivotItem],
+  shiny[div, moduleServer, NS, renderUI, tags],
+  shinyjs[useShinyjs],
+)
+
+box::use(
+  app/view[...],
 )
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  bootstrapPage(
-    uiOutput(ns("message"))
+  fluentPage(
+    useShinyjs(),
+    Pivot(
+      PivotItem(
+        headerText = "Wellcome",
+        "Global Greenhouse Gas Emissions Overview"
+      ),
+      PivotItem(
+        headerText = "A",
+        sidebar$ui(
+          id = ns("sidebar_A"),
+          name = "a_sidebar",
+          title = "Inputs",
+          sidebar_content = "Hello sidebar",
+          sidebar_bgc = "#ffff",
+          main_content = "Hello main",
+          main_bgc = "#DADAD9"
+        )
+      )
+    ),
+    style = "background-color:#ffff"
   )
 }
 
 #' @export
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    output$message <- renderUI({
-      div(
-        style = "display: flex; justify-content: center; align-items: center; height: 100vh;",
-        tags$h1(
-          tags$a("Check out Rhino docs!", href = "https://appsilon.github.io/rhino/")
-        )
-      )
-    })
+    ns <- session$ns
+    sidebar$server("sidebar_A")
+
   })
 }
